ab5bec5d989d4a5af6e1ce3d43b8663d8023e778
StanleyTC <rampeustiusken@gmail.com>
2023-07-04 19:37:23 -0300

    project 1 done

---
+++ project 1 - random name/main.py
@@ -0,0 +1,10 @@
+# INSTRUCTIONS:
+# Create a greeting for your program
+print('Hey there, welcome to the band name random generator!')
+# Ask the user the city he grew up in
+city = input('What city did you grow up in? ')
+# Ask the user the name of a pet
+pet = input('What is your pet name? ')
+# Combine the name of the city with the pet and show him his band name
+answer = city+" "+pet
+print(f'Your band name will be {answer}')
