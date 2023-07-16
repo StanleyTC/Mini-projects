f0328841969d8dc9ef2c70a08ec0f849fdbfb9aa
StanleyTC <rampeustiusken@gmail.com>
2023-07-16 14:48:01 -0300

    project 10 progress calculator

---
+++ python/project 10 - calculator/main.py
@@ -0,0 +1,11 @@
+from ascii import calculator
+print(calculator)
+firstValue = float(input('What is the first Number?: '))
+secondValue = float(input('What is the second Number?: '))
+print("""
++
+-
+*
+/
+""")
+value = input('pick an operation: ')
