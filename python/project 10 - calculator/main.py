54d4bb7f27473e00cf087601da860aa2bc0e58f4
StanleyTC <rampeustiusken@gmail.com>
2023-07-16 15:53:30 -0300

    calculator done

--- python/project 10 - calculator/main.py
+++ python/project 10 - calculator/main.py
@@ -1,11 +1,38 @@
 from ascii import calculator
+
+
+def calc(a, b, value):
+    if value == "*":
+        return a*b
+    elif value == "/":
+        return a/b
+    elif value == "+":
+        return a+b
+    else:
+        return a-b
+
+
+user = "yes"
+counter = 0
 print(calculator)
-firstValue = float(input('What is the first Number?: '))
-secondValue = float(input('What is the second Number?: '))
-print("""
-+
--
-*
-/
-""")
-value = input('pick an operation: ')
+
+
+while user == "yes":
+    if counter == 0:
+        firstValue = float(input('What is the first number?: '))
+    else:
+        firstValue = response
+    secondValue = float(input('What is the second number?: '))
+
+    print("""
+        +
+        -
+        *
+        /
+        """)
+    value = input('pick an operation: ')
+    response = calc(firstValue, secondValue, value)
+    print(f'The value is {response}')
+    user = input('Do you want to continue? yes/no: ').lower()
+    counter+=1
+print(f'The final value is {response}')
