fbc7679fa51be6987e47a372073ac6ae582dde6c
StanleyTC <rampeustiusken@gmail.com>
2023-07-17 07:50:53 -0300

    the number guessing complete

--- python/project 012 - the number guessing/main.py
+++ python/project 012 - the number guessing/main.py
@@ -3,38 +3,47 @@ from random import randint
 print("Welcome to the Number Guessing Game! \nI'm Thinking of a number between 1 and 100.")
 dificulty = input("Choose a difficulty. Type 'easy' or 'hard': ").lower()
 
-def compareNumber(user):
+def compareNumber(user, computer):
     """
-    function to compare user number with computer number. status = 1: user and computer have same number.
+    function to compare user number with computer number. 
+    status = 1: user and computer have same number.
     status = 0: they do not have same number and computer have the bigger
     status = -1: they do not have same number and user have the bigger
     """
-    computer = randint(1, 100)
     if user == computer:
         status = 1
-    elif user > computer:
+    if user > computer:
         status = -1
-    else:
+    if user < computer:
         status = 0
-        return status
+    return status
 
 
-
+computer = randint(1, 100)
 if dificulty == 'easy':
-    attempts = 5
-else:
     attempts = 10
+else:
+    attempts = 5
+
+
 
 while attempts != 0:
-
+    print(f'You have {attempts} attempts')
     user = int(input('Insert a number: '))
-    response = compareNumber(user)
-    if response == 0:
+
+    response=compareNumber(user, computer)
+
+    if response == 1:
+        print(f'Congratulations! you win, i was thinking in {computer}')
         break
-    elif response == -1:
+    if response == -1:
         print('Wrong, Try again, it is a lower number')
-        attempts =- 1
-    else:
+        attempts -= 1
+    if response == 0:
         print('Wrong, thy again, it is a bigger number')
-        attempts =- 1
-    
+        attempts -= 1
+
+
+
+if attempts == 0:
+    print('Sorry, you lost')
