f5cb5b1fb82a16dce3dbb2ad63bec0dbc8899d66
StanleyTC <rampeustiusken@gmail.com>
2023-07-19 15:22:47 -0300

    tips to do project 015

---
+++ python/project 015 - the coffe machine code/main.py
@@ -0,0 +1,27 @@
+# Fazer 3 sabores
+    # Expresso, Latte, Cappucino
+    # 50 ml Water/18g Coffee,   200 ml Water/24g Coffe/150ml Milk,      250ml Water/24g Coffee/100 ml Milk
+    # 1.5$, 2.5$, 3.0$
+
+    # Resources
+        # 300 ml water, 200 ml milk, 100g coffee
+
+# COIN OPERATED
+    # Penny = 1 cent
+    # Nickel = 5 Cents
+    # Dime = 10 cents
+    # Quarter = 25 cents
+
+
+
+"""
+What would you like? 3 options
+
+show resoucers and money with report command
+
+1 - print report
+2 - check resources sufficient
+3 - process coins
+4 - check transaction successful?
+5 - make coffee
+"""
