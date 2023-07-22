6f92f79c41b37797ae3e3b4d0decd3fc13a0b281
StanleyTC <rampeustiusken@gmail.com>
2023-07-22 10:25:31 -0300

    project 17 done

--- python/project 017 - the quizz game/main.py
+++ python/project 017 - the quizz game/main.py
@@ -1,14 +1,24 @@
 from data import question_data
 from random import randint
 from question_model import QuestionModel
+from replit import clear
+from time import sleep 
+from ascii import trophy, quiz
 
+
+clear()
+
+sleep(0.5)
+print(quiz)
 counter = 0
 correctAnswer = 0
+finalCounter = len(question_data)
 
-while counter < len(question_data):
+while counter < finalCounter:
     choose_quest = question_data[randint(0, len(question_data)-1)]
     question_data.remove(choose_quest)
     user = input(f"True or False - {choose_quest['text']}: ").capitalize()
+    sleep(0.25)
 
     question = QuestionModel(choose_quest, user)
     answer = question.verifyAnswer()
@@ -20,4 +30,15 @@ while counter < len(question_data):
 
     counter += 1
 
-print(f'Game over, you had {correctAnswer} of {len(question_data)}')
+
+if correctAnswer >= 10:
+    sleep(0.5)
+    print(f'Congratulations!, you had {correctAnswer} of {finalCounter}!, good results')
+    sleep(0.25)
+    print(trophy)
+elif correctAnswer >= 6:
+    sleep(0.5)
+    print(f'Good, satisfactory results, {correctAnswer} of {finalCounter}, keep going!')
+else:
+    sleep(0.5)
+    print(f'Sorry, you got {correctAnswer} of {finalCounter}, not good results')
