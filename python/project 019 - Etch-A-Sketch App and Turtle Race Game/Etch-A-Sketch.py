c6f2a0c552083ba4e16253edcb0dc3ecaef88ac3
StanleyTC <rampeustiusken@gmail.com>
2023-07-24 17:40:24 -0300

    sketch done

--- python/project 019 - Etch-A-Sketch App and Turtle Race Game/Etch-A-Sketch.py
+++ python/project 019 - Etch-A-Sketch App and Turtle Race Game/Etch-A-Sketch.py
@@ -3,6 +3,7 @@ from turtle import Turtle, Screen, done
 timmy = Turtle()
 screen = Screen()
 timmy.speed("fastest")
+timmy.color("darkblue")
 
 def move_forwards():
     timmy.forward(10)
@@ -22,11 +23,19 @@ def counter_clockwise():
     timmy.left(10)
 
 
+def clear():
+    timmy.clear()
+    timmy.penup()
+    timmy.home()
+    timmy.pendown()
+
+
 screen.listen() # To listen the button being used in keyboard
 screen.onkey(key="w", fun=move_forwards) # when space button be pressed, activate move_forwards function
 # when we use a function as a argument, we do not use () iin the function beeing called
 screen.onkey(key="s", fun=move_backwards)
 screen.onkey(key="d", fun=clockwise)
 screen.onkey(key="a", fun=counter_clockwise)
+screen.onkey(key="c", fun=clear)
 
 done()
