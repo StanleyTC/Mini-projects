8ee1f917114f1a14c6ef16801f251c14205797e7
StanleyTC <rampeustiusken@gmail.com>
2023-07-24 17:28:32 -0300

    project 19 progress etch-a-scketch game

---
+++ python/project 019 - Etch-A-Sketch App and Turtle Race Game/Etch-A-Sketch.py
@@ -0,0 +1,32 @@
+from turtle import Turtle, Screen, done
+
+timmy = Turtle()
+screen = Screen()
+timmy.speed("fastest")
+
+def move_forwards():
+    timmy.forward(10)
+
+
+def move_backwards():
+    timmy.right(180)
+    timmy.forward(10)
+    timmy.right(180)
+
+
+def clockwise():
+    timmy.right(10)
+
+
+def counter_clockwise():
+    timmy.left(10)
+
+
+screen.listen() # To listen the button being used in keyboard
+screen.onkey(key="w", fun=move_forwards) # when space button be pressed, activate move_forwards function
+# when we use a function as a argument, we do not use () iin the function beeing called
+screen.onkey(key="s", fun=move_backwards)
+screen.onkey(key="d", fun=clockwise)
+screen.onkey(key="a", fun=counter_clockwise)
+
+done()
---
+++ python/project 019 - Etch-A-Sketch App and Turtle Race Game/README.md
@@ -0,0 +1,3 @@
+# Etch-A-Sketch and Turtle Race Game 
+Continuing to explore the turtle library we used in project 18, the idea now is to build two little projects: a racing game between turtles and an etch-a-sketch game.
+the goal is to train the use of a function being sent as a parameter to another function, and for this we do not use the parentheses () is this second function, only the name of the function
