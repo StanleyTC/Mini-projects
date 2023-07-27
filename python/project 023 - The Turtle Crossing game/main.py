ebf2f2a6b0b0187b418624540b14258313cb4f6a
StanleyTC <rampeustiusken@gmail.com>
2023-07-27 10:36:18 -0300

    project 23 environment created

---
+++ python/project 023 - The Turtle Crossing game/main.py
@@ -0,0 +1,10 @@
+from turtle import Turtle, Screen, done
+
+# screen definitions
+# Screen definitions
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("white") # the screen will get the black color
+screen.title("Turtle Crossing game for StanleyTC")
+screen.tracer(0)
+done()
