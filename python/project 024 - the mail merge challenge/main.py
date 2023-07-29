a88191f1f9ee9144e12c83383d7f552fe71b1277
StanleyTC <rampeustiusken@gmail.com>
2023-07-29 14:23:42 -0300

    progress in automation

--- python/project 024 - the mail merge challenge/main.py
+++ python/project 024 - the mail merge challenge/main.py
@@ -1,1 +1,16 @@
-22464668523864486886486526645564645654565455555555555555555555555555555555
+names = []
+with open("Input/Names/invited_names.txt") as nomes:
+    for name in nomes:
+        names.append(name.strip())
+#print(names)
+
+with open("Input/Letters/starting_letter.txt") as carta:
+    content = carta.read()
+
+print(content)
+
+for name in names:
+    with open(f"Output/ReadyToSend/letter_for_{name}.txt", "w") as newFile:
+        content.replace("[name]", f"{name}")
+        newFile.write(content)
+        content.replace(f"{name}", "[name]")
