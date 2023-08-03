2607c177a582e7b1fe431006bd70be08e580d0c0
StanleyTC <rampeustiusken@gmail.com>
2023-08-03 16:35:32 -0300

    end of program

--- python/project 025 - U.S. States Game/main.py
+++ python/project 025 - U.S. States Game/main.py
@@ -24,6 +24,10 @@ while counter <51:
     new_counter += 1
     answer_state = screen.textinput(f"{corrects} of 50 states correct", "What is the state?").title()
 
+    # If user wants to end program before:
+    if answer_state == "Exit":
+        break
+
     if answer_state in all_states:
         corrects += 1
         names = turtle.Turtle()
@@ -34,8 +38,17 @@ while counter <51:
         state_data = data[data.state == answer_state]
         names.goto(int(state_data.x), int(state_data.y))
         names.write(answer_state)
+        all_states.remove(answer_state)
 
     counter+=1
 
+
+states_dict = {
+    "states missing: ": all_states
+}
+
+pandas_dict = pandas.DataFrame(states_dict)
+
+pandas_dict.to_csv("states to learn.csv")
 
 turtle.done()
